d7a38dc1af4e1c5c55975ebe9aaf9a340856947c
NAKS Chadiii <selfdiaz@gmail.com>
2024-07-25 13:04:39 +0200

    test http et https

--- ccg-project-client/config/httpServer.js
+++ ccg-project-client/config/httpServer.js
@@ -8,9 +8,15 @@
 const http = require('http');
 const https = require('https');
 
+const fs = require('fs');
+const options = {
+    key:fs.readFileSync('../ssl/privkey.pem'),
+    cert:fs.readFileSync('../ssl/allchange.pem')
+};
+
 const app = require('../controllers/index.js');
 
 module.exports.http = http.createServer(app);
-module.exports.https = https.createServer(app);
+module.exports.https = https.createServer(options, app);
 //import { createServer } from "http";
 //import app from './../controllers/index.js';
