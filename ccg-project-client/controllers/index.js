df80159d3f2ef84473b52917d6a89509d86e0bdb
NAKS Chadiii <selfdiaz@gmail.com>
2024-07-26 14:22:18 +0200

    Update routes

--- ccg-project-client/controllers/index.js
+++ ccg-project-client/controllers/index.js
@@ -10,6 +10,9 @@ const __dirnameX = require("../config/__dirname.js");
 
 for (const page of pages) app.get(page, (req, res) => res.sendFile(path.join(__dirnameX, 'dist', 'index.html')));
 app.get('/boutique-sevres', (req, res) => res.redirect('/sevres'));
+app.get('/copycat/group', (req, res) => res.redirect('/about'));
+app.get('/a-propos', (req, res) => res.redirect('/about'));
+
 //app.get('/*', (req, res) => res.redirect('/'));
 
 module.exports = app;
