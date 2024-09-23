c32ac81677732ef566ea9d7c5f60dbe39da1f899
nakschadiii <105643348+nakschadiii@users.noreply.github.com>
2024-09-23 15:11:37 +0200

    correction secure

--- ccg-project-client/CCG_MASTER.js
+++ ccg-project-client/CCG_MASTER.js
@@ -3,5 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const { PORT, PORT2 } = process.env;
+
 server.listen(PORT, () => { console.log('listening on *:'+PORT); });
 serverSecure.listen(PORT2, () => { console.log('listening on *:'+PORT2); })
