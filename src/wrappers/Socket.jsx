bef6a1c0ac1628e7e4495453e52ad175e097fdd3
nakschadiii <105643348+nakschadiii@users.noreply.github.com>
2024-09-23 15:47:34 +0200

    correction secure

--- src/wrappers/Socket.jsx
+++ src/wrappers/Socket.jsx
@@ -1,6 +1,7 @@
 import { createContext } from 'react';
 import { io } from 'socket.io-client';
-export const socket = io(`http://${window.location.hostname}`, { withCredentials: true });
+
+export const socket = io(`${window.location.protocol}//${window.location.hostname}`, { withCredentials: true });
 export const SocketContext = createContext(null);
 
 export default function ({ children }) {
