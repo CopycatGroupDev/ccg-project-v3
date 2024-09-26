43583ccd23a80ecbd22161f91a3aeb3e098d6ca9
nakschadiii <105643348+nakschadiii@users.noreply.github.com>
2024-09-26 11:01:21 +0200

    corrections liens morts

---
+++ ccg-project-client/config/reactRoutes.js
@@ -0,0 +1,22 @@
+export default [
+	'/',
+	'/labs',
+	'/shop',
+	'/print',
+	'/solutions',
+	'/contact',
+	'/about',
+	'/rse',
+	'/comptoirs',
+	'/sevres',
+	'/boulogne',
+	'/mentions',
+	'/confidentialit%C3%A9'
+]
+
+export const toRedirect = [
+	{ oldPath : '/boutique-sevres', newPath : '/sevres' },
+	{ oldPath : '/a-propos', newPath : '/about' },
+	{ oldPath : '/copycat/group', newPath : '/about' },
+	{ oldPath : '/s%C3%A8vres', newPath : '/sevres' }
+]
--- ccg-project-client/config/routes.js
+++ ccg-project-client/config/routes.js
@@ -1,7 +1,10 @@
 import react from '../controllers/reactView.js';
+import reactRoutes, { toRedirect } from './reactRoutes.js';
 
 export default [
-	{ path : '/', method : 'get', action : react, },
+	...reactRoutes.map(path => ({ path, method : 'get', action : react, })),
+	...toRedirect.map(({ oldPath, newPath }) => ({ path : oldPath, method : 'get', action : (req, res) => res.redirect(newPath), })),
 	//{ path : '**', method : 'get', action : (req, res) => res.redirect("/"), },
 ]
 
+///s%C3%A8vres
