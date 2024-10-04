79b1d8ad5ebc6fc87cd677af3eca7352facb219c
nakschadiii <105643348+nakschadiii@users.noreply.github.com>
2024-10-04 17:39:25 +0200

    test path

--- dist-srv/config/httpsCreateServer.js
+++ dist-srv/config/httpsCreateServer.js
@@ -2,8 +2,10 @@ import app from './app.js';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
+
 dotenv.config();
 const { dev_mode, ssl_key, ssl_cert } = process.env;
+console.log(path.resolve(import.meta.url, ssl_key));
 console.log(dev_mode, typeof dev_mode);
 const options = JSON.parse(dev_mode) ? {} : {
     key:fs.readFileSync(ssl_key),
@@ -11,4 +13,5 @@ const options = JSON.parse(dev_mode) ? {} : {
 };
 
 import { createServer } from 'https';
+import path from 'path';
 export default createServer(options, app);
